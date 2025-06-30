d1f9347c48edeb42722a719b46df93d80d535b4f
surasanthosh <suraa.santhoshh@gmail.com>
2025-06-30 12:32:32 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int a=10;
         int A=12;
         int $a=13;
-        System.out.println(a);
         System.out.println(A);
         System.out.println($a);
     }
